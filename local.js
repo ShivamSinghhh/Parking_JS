4692c0878abec499b1c19c012cb585aabb188740
Shivam Singh <shivam_fw12_029@masai.school>
2022-03-10 03:03:38 +0530

    vehicle number verified

--- local.js
+++ local.js
@@ -19,16 +19,16 @@ let parkingZone = {
 
 localStorage.setItem("parkingZone",JSON.stringify(parkingZone))
 
-let carSlot = new Array(parkingZone.car.max).fill({ "avail": true, "username":null, "userId": null, "entry": null, "exit": null, "rate": null });
+let carSlot = new Array(parkingZone.car.max).fill({ "avail": true, "username":null, "userId": null, "entry": null, "exit": null,"vehicle_No": null});
 
 localStorage.setItem("carSlot",JSON.stringify(carSlot))
 
-let bikeSlot = new Array(parkingZone.bike.max).fill({ "avail": true, "username":null, "userId": null, "entry": null, "exit": null, "rate": null });
+let bikeSlot = new Array(parkingZone.bike.max).fill({ "avail": true, "username":null, "userId": null, "entry": null, "exit": null,"vehicle_No": null});
 
 localStorage.setItem("bikeSlot",JSON.stringify(bikeSlot))
 
 
-let truckSlot = new Array(parkingZone.truck.max).fill({ "avail": true, "username":null, "userId": null, "entry": null, "exit": null, "rate": null });
+let truckSlot = new Array(parkingZone.truck.max).fill({ "avail": true, "username":null, "userId": null, "entry": null, "exit": null,"vehicle_No": null});
 
 localStorage.setItem("truckSlot",JSON.stringify(truckSlot))
 
