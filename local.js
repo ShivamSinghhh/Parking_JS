7744578a17fe0fac35c488720931207c5d789336
Shivam Singh <shivam_fw12_029@masai.school>
2022-03-09 13:35:00 +0530

    recheck

--- local.js
+++ local.js
@@ -1,19 +1,4 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
-
-<script>
-
     
 let parkingSchema = {
     "bike": {
@@ -48,7 +33,7 @@ localStorage.setItem("tempoSlot",JSON.stringify(tempoSlotSchema))
 
 
     
-</script>
+
 
 
 
